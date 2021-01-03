91622079eb2983e9ca8458296ac4d477fff20b63
Danilo Velasquez Urrutia <danilo.velasquez@gmail.com>
2021-01-03 18:48:38 +0100

    ci(lighthouse-ci) (#10)
    * ci(lighthouse-ci): added lighthouse-ci.yml to test site after deploy to s3
    
    * ci(lighthouse-ci): removed unnused files
    updated ci to use yarn --frozen-lockfile
    
    * ci(lighthouse-ci): test comment on PR
    
    * ci(lighthouse-ci): test comment on PR
    
    * ci(lighthouse-ci): reverted lost changes
    
    * ci(lighthouse-ci): added file as script instead of inline js
    
    * ci(lighthouse-ci): fixed caps and route
    
    * ci(lighthouse-ci): test github and context objects
    
    * ci(lighthouse-ci): fixed result for multiple pages
    
    * ci(lighthouse-ci): updated budget.json to match lighthouse recommendations
    
    * ci(lighthouse-ci): added LHCI_GITHUB_TOKEN
    
    Co-authored-by: Danilo Velasquez <null>

---
+++ .github/workflows/scripts/format-lighthouse-score.js
@@ -0,0 +1,28 @@
+const createSingleComment = ({ summary, testUrl, reportPublicUrl }) => {
+  const formatResult = (res) => Math.round((res * 100));
+  Object.keys(summary).forEach(key => summary[key] = formatResult(summary[key]));
+  const score = res => res >= 90 ? "🟢" : res >= 50 ? "🟠" : "🔴";
+  return [
+    `⚡️ [Lighthouse report](${reportPublicUrl}) for the changes in this PR:`,
+    "| Category | Score |",
+    "| --- | --- |",
+    `| ${score(summary.performance)} Performance | ${summary.performance} |`,
+    `| ${score(summary.accessibility)} Accessibility | ${summary.accessibility} |`,
+    `| ${score(summary["best-practices"])} Best practices | ${summary["best-practices"]} |`,
+    `| ${score(summary.seo)} SEO | ${summary.seo} |`,
+    `| ${score(summary.pwa)} PWA | ${summary.pwa} |`,
+    " ",
+    `*Lighthouse ran on [${testUrl}](${testUrl})*`
+  ].join("\n")
+}
+
+
+module.exports = ({ results, links }) => {
+  return results.map(result =>{
+    const testUrl = Object.keys(links).find(key => key === result.url)
+    const reportPublicUrl = links[testUrl]
+    return createSingleComment({summary: result.summary, testUrl, reportPublicUrl})
+  }).join("\n")
+};
+
+
