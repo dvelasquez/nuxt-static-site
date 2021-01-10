39f31043701f4617612fbefa5c254726c093ad96
Danilo Velasquez <null>
2021-01-10 20:58:01 +0100

    feat(google-analytics): updated nuxt config disabling analyze and disabling theme config added vuetify theme css data to variables.scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,3 @@
-import colors from 'vuetify/es5/util/colors'
-
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
@@ -67,18 +65,7 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     defaultAssets: false,
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3,
-        },
-      },
+      disable: true,
     },
   },
 
@@ -106,7 +93,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    analyze: true,
+    analyze: false,
     parallel: false,
     extractCSS: true,
     splitChunks: {
