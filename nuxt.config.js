0d74d19b5a76d07455af08e4ac45677d91f70e58
Danilo Velasquez <null>
2021-01-10 21:05:34 +0100

    feat(google-analytics): rolled back color change

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import colors from 'vuetify/es5/util/colors'
+
 export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
@@ -65,7 +67,18 @@ export default {
     customVariables: ['~/assets/variables.scss'],
     defaultAssets: false,
     theme: {
-      disable: true,
+      dark: true,
+      themes: {
+        dark: {
+          primary: colors.blue.darken2,
+          accent: colors.grey.darken3,
+          secondary: colors.amber.darken3,
+          info: colors.teal.lighten1,
+          warning: colors.amber.base,
+          error: colors.deepOrange.accent4,
+          success: colors.green.accent3,
+        },
+      },
     },
   },
 
@@ -93,7 +106,7 @@ export default {
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
-    analyze: false,
+    analyze: true,
     parallel: false,
     extractCSS: true,
     splitChunks: {
