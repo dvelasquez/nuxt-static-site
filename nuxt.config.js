b96b529fbc09179a1611d0da56deeeff2ff64363
Danilo Velasquez <null>
2021-01-10 15:13:05 +0100

    feat(google-analytics): update code to install google analytics through a plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,7 +20,11 @@ export default {
   css: [],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['@/plugins/composition-api'],
+  plugins: [
+    '@/plugins/composition-api',
+    // Google Analytics setup https://nuxtjs.org/faq/ga
+    { src: '~plugins/ga.js', mode: 'client' },
+  ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
